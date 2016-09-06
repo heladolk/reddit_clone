3bad57268192d8193e138691bfce6107b4ae3a70
heladolk <heladolk@yahoo.com>
2016-09-06 06:23:53 -0700

    add search capability

--- js/main.js
+++ js/main.js
@@ -77,7 +77,7 @@ app.controller('postController', function($scope) {
         Comment Feature
     */
     $scope.toggleCommentsVisibility = function(post) {
-        if(post.numComments === 0) {
+        if(post.comments.length === 0) {
             return;
         }
         post.commentsVisible = !post.commentsVisible ;
@@ -89,7 +89,6 @@ app.controller('postController', function($scope) {
 
     $scope.submitNewComment = function(post, comment) {
         post.comments.push(comment);
-        post.numComments += 1;
         post.addcommentVisible = false;
         $scope.newComment = {};
         $scope.commentForm.$setPristine();
@@ -104,7 +103,6 @@ app.controller('postController', function($scope) {
         imgurl: 'http://static4.businessinsider.com/image/55759ecb6da8116622b7ee96-806-604/pizza-hut-blockbuster-box-movie-projector.jpg',
         description: "Thinking that movie nights and pizza are a natural fit, Pizza Hut created a new cardboard pizza box that turns into a working movie projector powered by your smartphone. It's called the Blockbuster Box, and it was designed by Ogilvy Hong Kong for a Pizza Hut marketing stunt, according to The Verge.",
         voteCount: 7,
-        numComments: 2,
         comments: [ {author: 'chul', content: 'handsome'}, {author: 'yj', content: 'pretty'}],
         datePosted: '09/04/16',
         commentsVisible: false,
@@ -116,7 +114,6 @@ app.controller('postController', function($scope) {
         imgurl: 'https://cdn2.vox-cdn.com/thumbor/b6qsUNzGyTjU-M4wJKLoC5L5HKg=/0x0:2961x1974/1920x1280/filters:format(webp)/cdn0.vox-cdn.com/uploads/chorus_image/image/50495517/493360723.0.jpg',
         description: "Silicon Valley has a major housing crisis. The median price of a home in Palo Alto, home to Stanford University, has reached $2.5 million — twice as much as five years ago. Housing in the region has grown so expensive that it’s hard for young families to get their start there.",
         voteCount: 5,
-        numComments: 0,
         comments: [],
         datePosted: '09/02/16',
         commentsVisible: false,
@@ -128,7 +125,6 @@ app.controller('postController', function($scope) {
         imgurl: 'http://static.independent.co.uk/s3fs-public/styles/story_large/public/thumbnails/image/2016/09/02/08/norway-church.jpg',
         description: "Norway's state church has lost more than 25,000 members in a month after it launched an online registration system allowing people to sign up or opt out. The Lutheran Church is the official religion in Norway and nearly three-quarters of the population are registered as members. However, since launching a website on 12 August to update its heavily criticised records and offer people an easy way to join or leave, 25,743 members have left the church, 24,653 of those through the site.",
         voteCount: 0,
-        numComments: 0,
         comments: [],
         datePosted: '08/31/16',
         commentsVisible: false,
